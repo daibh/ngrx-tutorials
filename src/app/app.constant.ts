ac555203b1c4b0c8a3cfcd9b0c3bd0f3a4fbc906
daibh <oniwa1102@gmail.com>
2019-04-24 01:14:58 +0700

    update code

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,7 +1,7 @@
-export const SERVER_API_URL = 'http://192.168.50.13:50000/api';
+export const SERVER_API_URL = 'https://huudai.com/api';
 
 // Account Api Constant
-export const API_ACCOUNT_LOGIN = 'Account/Login'; 
-export const API_ACCOUNT_GET_MENU_LIST = 'Account/GetMenuList';
-export const API_ACCOUNT_GET_USER = 'Account/GetUser';
-export const API_ACCOUNT_GET_USER_LIST = 'Account/GetListUser';
+export const API_ACCOUNT_LOGIN = 'Login'; 
+export const API_ACCOUNT_GET_MENU_LIST = 'account/menu';
+export const API_ACCOUNT_GET_USER = 'account';
+export const API_ACCOUNT_GET_USER_LIST = 'account/GetListUser';
